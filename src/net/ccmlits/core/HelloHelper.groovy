bc525e0556a6df7f4a3490cd1951306f8bd4aef5
cca <claude.cato@proximus.be>
2023-10-17 23:15:21 +0200

    test

--- src/net/ccmlits/core/HelloHelper.groovy
+++ src/net/ccmlits/core/HelloHelper.groovy
@@ -1,5 +1,37 @@
 package bet.ccmlits.core
 
+/** A map that holds all constants and data members that can be override when constructing  */
+helloHelperEnv
+
+/**
+ * Imitates a constructor
+ * Defines an instance of HelloHelper object. All according to api
+ * @param hiWord - word that say hello
+ * @param helloSentenceEnd - end of the hello sentence
+ * @return
+ */
+def construct(String hiWord, String helloSentenceEnd){
+
+    helloHelperEnv = [
+            hiWord : hiWord,
+            helloSentenceEnd: helloSentenceEnd,
+            defaultName: 'World'
+
+    ]
+}
+
+def changeDefaultName(String name) {
+    HelloHelper.put('defaultName', name)
+}
+
+def sayHelloSentence(String name) {
+    if(!name) {
+        echo "${helloHelperEnv.hiWord}, ${helloHelperEnv.defaultName} ${helloHelperEnv.helloSentenceEnd}"
+    } else {
+        echo "${helloHelperEnv.hiWord}, ${name} ${helloHelperEnv.helloSentenceEnd}"
+    }
+}
+
 def sayHello1FromClass(name) {
   sayHello2FromClass("${name} (1)")
 }
