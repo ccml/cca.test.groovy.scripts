4e2b368fa277afa2281dc180ae78b7ed3dcaef1e
cca <claude.cato@proximus.be>
2023-10-20 23:52:46 +0200

    test

--- vars/tplPipeline.groovy
+++ vars/tplPipeline.groovy
@@ -20,7 +20,7 @@ def call(Map config) {
             stage ('before') {
                 steps {
                     script {
-                        config.beforeHook()
+                        config.beforeHook(this)
                     }
                 }
             }
